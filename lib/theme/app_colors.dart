69d1b67c619245a7cc0225ec5081da371088a8d8
Lukas <lukas581800@gmail.com>
2026-06-21 17:28:51 -0300

    novas cores

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppColors {
+  static const background = Color(0xFFFFF8F6);
+  static const primary = Color(0xFFD8A7B1);
+  static const secondary = Color(0xFF8B6F8E);
+  static const text = Color(0xFF2D2D2D);
+  static const highlight = Color(0xFFE8C4C4);
+
+  static const surface = Color(0xFFFFFFFF);
+  static const mutedText = Color(0xFF6F626A);
+  static const border = Color(0x33D8A7B1);
+}
